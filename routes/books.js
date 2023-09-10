705614aae8c75024549c07a21be7c55ca2d5c4b6
NicolasV22 <135312651+NicolasV22@users.noreply.github.com>
2023-09-10 12:49:29 +0200

    Add files via upload

--- routes/books.js
+++ routes/books.js
@@ -8,12 +8,12 @@ const multer = require('../middleware/multer-config')
 /* Intervention du middleware d'authentification dans les routes où il est requis (creation/modification/suppresion/notation d'un livre ) */
 /* Intervention du middleware multer dans book post et put : il sert à gérer l'image envoyée, et dans la conversion de celle-ci via le controller */
 
-router.get('/',  bookCtrl.getAllBooks);
-router.post('/', auth, multer,  bookCtrl.createBook);
-router.get('/bestrating', bookCtrl.bestRatedBooks);
-router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
-router.delete('/:id', auth , bookCtrl.deleteBook);
-router.post('/:id/rating', auth, bookCtrl.bookRating);
+router.get('/',  bookCtrl.getAllBooks);     /* route d'affichage de tous les livres */
+router.post('/', auth, multer,  bookCtrl.createBook);     /* route d'ajout d'un livre */   
+router.get('/bestrating', bookCtrl.bestRatedBooks);     /* route d'import des 3 meilleurs livre lors de l'ouverture d'un livre */
+router.get('/:id', bookCtrl.getOneBook);     /* route d'affichage d'un seul livre lorsqu'on clique dessus */
+router.put('/:id', auth, multer, bookCtrl.modifyBook);     /* route de modification d'un livre */
+router.delete('/:id', auth , bookCtrl.deleteBook);     /* route de suppression d'un livre */
+router.post('/:id/rating', auth, bookCtrl.bookRating);     /* route d'ajout d'une note à un livre par un user */
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const userCtrl = require('../controllers/user')
 
 /* Routes user, connexion et inscription */
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', userCtrl.signup);      /* Route d'inscription */
+router.post('/login', userCtrl.login);     /* Route de connexion */
 
 module.exports = router;
